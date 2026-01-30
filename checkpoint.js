7a0679cd198a736bc865795c82cd53c19d8e4ee0
HAMZA <hamza.zerouki11@gmail.com>
2026-01-30 16:19:19 +0100

    Initial commit

---
+++ checkpoint.js
@@ -0,0 +1,54 @@
+// Lavel : Easy
+// Given an array of numbers, return the first element in the array.
+function getFirstValue(arr) {
+  return arr[0];
+}
+
+getFirstValue([100, 5, 50]); // 100
+
+// Lavel : Medium
+function addition(num) {
+  return num + 1;
+}
+// Lavel : Hard
+function circuitPower(voltage, current) {
+  return voltage * current;
+}
+// Lavel : Expert
+function sumPolygon(n) {
+  return (n - 2) * 180;
+}
+// Lavel : Master
+function squared(b) {
+  return b * b;
+}
+// Lavel : Grandmaster
+function giveMeSomething(a) {
+  return "something " + a;
+}
+// Lavel : Legendary
+function printArray(number) {
+  var newArray = [];
+
+  for (var i = 1; i <= number; i++) {
+    newArray.push(i);
+  }
+
+  return newArray;
+}
+// Lavel : Mythical
+function nextEdge(side1, side2) {
+  return side1 + side2 - 1;
+}
+// Lavel : Divine
+function remainder(x, y) {
+  return x % y;
+}
+// Lavel : Celestial
+function lessThan100(a, b) {
+  return (a + b) < 100;
+}
+// Lavel : Transcendent
+function convert(minutes) {
+  return minutes * 60;
+}
